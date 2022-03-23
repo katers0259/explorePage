2582bd836eb81cc58ad3b5e22b498986151a6734
CBranagan <chad.branagan@gmail.com>
2022-03-23 14:29:04 -0600

    finished up mobile and modal

---
+++ modal.js
@@ -0,0 +1,10 @@
+
+
+function fadeText(){
+    document.getElementById('fade-explore').style.color = 'transparent';
+}
+
+function bringBack() {
+    document.getElementById('fade-explore').style.color = 'white';
+}
+
